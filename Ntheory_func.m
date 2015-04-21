e65d264089aa80493d5db4dadcf5fe1a22d7974a
nmosk <mosknj@gmail.com>
2015-04-20 21:14:03 -0700

    made faster and changed call sign - Amanda

--- Ntheory_func.m
+++ Ntheory_func.m
@@ -4,24 +4,25 @@
 % uses the Gilliland design method equation (eqn 4.56)
 % also outputs a plot of (N-N_min)/(N+1) vs (r-r_min)/(r+1)
 
-function [ N_theory_array, r_array, N_theory ] = Untitled2( r_min, N_min, r )
-
-r_max = (.95+r_min)/(1-.95); r_array =r_min:(r_max-r_min)/1000:r_max; % so that we go to nearly 0 to 1 on the x axis
-N_theory_array = [];
-y_array = [];
-
-for n = 1:length(r_array)
-syms N_theory_temp
-N_theory_temp = solve( (N_theory_temp-N_min)/(N_theory_temp+1) == 0.75 .* (1 - ((r_array(n)-r_min)./(r_array(n)+1))^0.5688) , N_theory_temp);
-y_value = (N_theory_temp-N_min)/(N_theory_temp+1);
-N_theory_array(n) = N_theory_temp;
-y_array(n) = y_value;
-end
-x_array = (r_array-r_min) ./ (r_array+1);
+% function [ N_theory_array, r_array, N_theory ] = Ntheory_func( r_min, N_min, r )
+function [ N_theory ] = Ntheory_func( r_min, N_min, r )
+% r_max = (.95+r_min)/(1-.95); r_array =r_min:(r_max-r_min)/1000:r_max; % so that we go to nearly 0 to 1 on the x axis
+% N_theory_array = [];
+% y_array = [];
+% 
+% for n = 1:length(r_array)
+% syms N_theory_temp
+% N_theory_temp = solve( (N_theory_temp-N_min)/(N_theory_temp+1) == 0.75 .* (1 - ((r_array(n)-r_min)./(r_array(n)+1))^0.5688) , N_theory_temp);
+% y_value = (N_theory_temp-N_min)/(N_theory_temp+1);
+% N_theory_array(n) = N_theory_temp;
+% y_array(n) = y_value;
+% end
+% x_array = (r_array-r_min) ./ (r_array+1);
 syms N_theory
 N_theory = solve( (N_theory-N_min)/(N_theory+1) == 0.75 .* (1 - ((r-r_min)./(r+1))^0.5688) , N_theory);
 
-figure
-hold on;
-plot(x_array,y_array)
-axis( [ 0, 1, 0, 1]);
+% figure
+% hold on;
+% plot(x_array,y_array)
+% axis( [ 0, 1, 0, 1]);
+end
