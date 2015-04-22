518348c0c5cfc5542620372407edfb7a448244dc
nmosk <mosknj@gmail.com>
2015-04-21 20:30:02 -0700

    one year of utilities!

--- CostModel_func.m
+++ CostModel_func.m
@@ -3,7 +3,7 @@ function [ costs, costs_check ] = CostModel_func(N_real, V)
 % Costs: Co*N^0.8*V^0.5 + C1*V^0.65+C2*V
 
 C = [1*10^3 1*10^5 1*10^4];
-costs = (C(1)*N_real^0.8*V^0.5) + (C(2)*V^0.65) + (C(3)*V*10); % where 10 is 10 years because the third term is $/yr
+costs = (C(1)*N_real^0.8*V^0.5) + (C(2)*V^0.65) + (C(3)*V); % where 10 is 10 years because the third term is $/yr
 costs1 = (C(1)*N_real^0.8*V^0.5);
 costs2 = (C(2)*V^0.65);
 costs3 = (C(3)*V);
