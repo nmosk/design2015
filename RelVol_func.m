b4c0f9ecbd8737a14849e68c827bb1d1ae686c23
nmosk <mosknj@gmail.com>
2015-04-21 19:30:50 -0700

    now does relevant volatitlities in vector

--- RelVol_func.m
+++ RelVol_func.m
@@ -4,10 +4,10 @@
 function [RelVol] = RelVol_func(T)
 
 T = T-273.15; % [C]
-RelVol_be = vap_P(T,'be')/vap_P(T,'st')
-RelVol_to = vap_P(T,'to')/vap_P(T,'st')
-RelVol_eb = vap_P(T,'eb')/vap_P(T,'st')
-RelVol = [RelVol_be; RelVol_to; RelVol_eb]
+RelVol_be_to = vap_P(T,'be')/vap_P(T,'to')
+RelVol_to_eb = vap_P(T,'to')/vap_P(T,'eb')
+RelVol_eb_st = vap_P(T,'eb')/vap_P(T,'st')
+RelVol = [RelVol_be_to; RelVol_to_eb; RelVol_eb_st]
 
 TT = 10:200;
 plot(TT,vap_P(TT,'be')./vap_P(TT,'st'),TT,vap_P(TT,'to')./vap_P(TT,'st'),TT,vap_P(TT,'eb')./vap_P(TT,'st'))
